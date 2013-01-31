4386698ca16f1dc42e13954ccf65f76173f99e23
Tekin Suleyman <tekin@tekin.co.uk>
2013-01-31 17:55:37 +0000

    consistency in tests...

--- test/test_eventful_api.rb
+++ test/test_eventful_api.rb
@@ -5,12 +5,12 @@ class TestEventfulApi < MiniTest::Unit::TestCase
   describe EventfulApi do
     it 'is configurable' do
       EventfulApi.configure do |config|
-        config.consumer_key = 'some key'
-        config.consumer_secret = 'some secret'
+        config.consumer_key = 'key'
+        config.consumer_secret = 'secret'
       end
 
-      assert_equal 'some key', EventfulApi.config.consumer_key
-      assert_equal 'some secret', EventfulApi.config.consumer_secret
+      assert_equal 'key', EventfulApi.config.consumer_key
+      assert_equal 'secret', EventfulApi.config.consumer_secret
     end
 
     describe 'once configured' do

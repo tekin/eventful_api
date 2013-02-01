e5bcdf6ec6d1400187419eb73ed01e02d38f808c
Tekin Suleyman <tekin@tekin.co.uk>
2013-02-01 11:23:24 +0000

    Client needs a different consumer config to

--- lib/eventful_api/client.rb
+++ lib/eventful_api/client.rb
@@ -3,10 +3,12 @@ require 'multi_json'
 
 module EventfulApi
   class Client
-    attr_reader :access_token
+    API_URL = 'http://api.eventful.com'
+    attr_reader :access_token, :oauth_consumer
 
     def initialize(params)
-      @access_token = OAuth::AccessToken.new(EventfulApi.oauth_consumer, params[:oauth_token], params[:oauth_secret])
+      @oauth_consumer = OAuth::Consumer.new(EventfulApi.config.consumer_key, EventfulApi.config.consumer_secret, :site => API_URL, :scheme => EventfulApi::SCHEME)
+      @access_token = OAuth::AccessToken.new(oauth_consumer, params[:oauth_token], params[:oauth_secret])
     end
 
     def get(method, params)
--- test/test_client.rb
+++ test/test_client.rb
@@ -1,24 +1,40 @@
 require 'test_helper'
 
 class TestEventfulApi < MiniTest::Unit::TestCase
+  def setup
+    EventfulApi.configure do |config|
+      config.consumer_key = 'key'
+      config.consumer_secret = 'secret'
+      config.app_key = 'app_key'
+    end
+  end
 
   describe EventfulApi::Client do
     before do
-      @client = EventfulApi::Client.new(:oauth_token => 'key', :oauth_secret => 'secret')
+      @client = EventfulApi::Client.new(:oauth_token => 'client_key', :oauth_secret => 'client_secret')
     end
 
-    it 'has an OAuth::AccessToken' do
+    it 'has a oauth consumer for accessing the Eventful API' do
+      consumer = @client.oauth_consumer
+
+      assert consumer.is_a?(OAuth::Consumer)
+      assert_equal EventfulApi::Client::API_URL, consumer.site
+      assert_equal EventfulApi.config.consumer_key, consumer.key
+      assert_equal EventfulApi.config.consumer_secret, consumer.secret
+    end
+
+    it 'has an ouath access token' do
       access_token = @client.access_token
 
       assert access_token.is_a?(OAuth::AccessToken)
-      assert_equal 'key', access_token.token
-      assert_equal 'secret', access_token.secret
-      assert_equal EventfulApi.oauth_consumer, access_token.consumer
+      assert_equal 'client_key', access_token.token
+      assert_equal 'client_secret', access_token.secret
+      assert_equal @client.oauth_consumer, access_token.consumer
     end
 
     describe 'making a GET API call with parameters' do
 
-      it 'transform and delegate the request to the access token' do
+      it 'transforms the request and delegates to the access token' do
         @client.access_token.expects(:get).with('/json/events/get?id=E0-001-053639493-9').returns(mock_response)
         @client.get('/events/get', :id => 'E0-001-053639493-9')
       end
